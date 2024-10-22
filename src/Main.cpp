72f7fce15da3a27123bab4ab2176da027a2cedcb
Pyritium <68076312+Pyritium@users.noreply.github.com>
2024-10-22 02:17:28 -0500

    Initial commit

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <Windows.h>
 
-int main()
+HHOOK KeyboardHook;
+
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	std::cout << nCode << std::endl;
+	return CallNextHookEx(KeyboardHook, nCode, wParam, lParam);
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 
-}
+	// Some debug tools to see processes
+	AllocConsole();
+
+	FILE* console;
+	freopen_s(&console, "CONOUT$", "w", stdout);
+	freopen_s(&console, "CONOUT$", "w", stderr);
+
+	KeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+};
