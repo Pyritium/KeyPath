41183f451b0339a786364616f4ae0b6d1347354d
Pyritium <68076312+Pyritium@users.noreply.github.com>
2024-10-23 20:11:17 -0500

    Added WindowProc, set up WNDCLASS CIM

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,11 +3,23 @@
 
 HHOOK KeyboardHook;
 
+
 LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-	std::cout << nCode << std::endl;
+	std::cout << wParam << std::endl;
 	return CallNextHookEx(KeyboardHook, nCode, wParam, lParam);
 };
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+};
+
+const LPCWSTR CIM_CLASS = L"CIM";
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
@@ -20,6 +32,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	freopen_s(&console, "CONOUT$", "w", stderr);
 
 	KeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
+	
+	// Register WNDCLASS CIM
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CIM_CLASS;
+	RegisterClass(&wc);
+
+	
+	HWND Window = CreateWindowEx(0, CIM_CLASS, L"Custom Input Manager", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU, CW_USEDEFAULT, CW_USEDEFAULT, 500, 300, NULL, NULL, hInstance, NULL);
+	
+	ShowWindow(Window, SW_SHOW);
+	UpdateWindow(Window);
 
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0)) {
